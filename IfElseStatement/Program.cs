2d064a78193c9c108391e29c3ee7783f018ede92
msimangoksi@gmail.com <msimangoksi@gmail.com>
2023-04-25 14:48:03 +0200

    Add project files.

---
+++ IfElseStatement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = 12;
+            string weather = "bad";
+
+            if (people <= 10 && weather == "nice")
+            {
+                Console.WriteLine("SaladMart");
+            }
+            else
+            {
+                Console.WriteLine("Soup N Sandwich");
+            }
+        }
+    }
+}
+
